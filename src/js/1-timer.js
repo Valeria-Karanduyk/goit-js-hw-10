7e03fc10ed10408d39cec10df40d662ac38b9a45
Valeria_Karanduyk <lera-ka0195@ukr.net>
2024-09-11 21:44:33 +0300

    timer js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,1 +1,25 @@
-<a class="back-link" href="./index.html">Go back</a>
+'use strict';
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const refs = {
+  input: document.querySelector('#datetime-picker'),
+  start: document.querySelector('button[data-start]'),
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  mins: document.querySelector('span[data-minutes]'),
+  secs: document.querySelector('span[data-seconds]'),
+};
+
+let intervalId = null;
+refs.start.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
